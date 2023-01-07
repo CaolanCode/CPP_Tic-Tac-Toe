7044e7cbe56d6fc16d59a4e18295fa94b7b8f732
CaolanCode <cmacmahon3@gmail.com>
2023-01-07 17:46:33 +0000

    commit creation of board

--- main.cpp
+++ main.cpp
@@ -1,28 +1,60 @@
 /*
     Tic-Tac-Toe
  */
+
 #include <iostream>
 using namespace std;
 
-class TicTacToe
+void createBoard(char (&fullBoard)[11][11]);
+void printBoard(char (&fullBoard)[11][11]);
+void updateBoard(char (&fullBoard)[11][11], char (&choices)[9]);
+
+int main()
 {
-    char marker;
-public:
-    TicTacToe(char m)
-    {
-        cout << "In constructor\n";
-        //setName(n);
-        marker = m;
-        cout << marker << endl;
-    }
-    char get_marker() { return marker;}
-};
-int main() {
-    TicTacToe playerX('X');
-    TicTacToe playerY('O');
+    char fullBoard[11][11];
+    char choices[] = {'1','2','3','4','5','6','7','8','9'};
     
-    cout << playerX.get_marker() << endl;
-    cout << playerY.get_marker() << endl;
+    createBoard(fullBoard);
+    updateBoard(fullBoard, choices);
+    printBoard(fullBoard);
     
     return 0;
+}
+
+void createBoard(char (&fullBoard)[11][11])
+{
+    for(int i = 0; i < 11; i++)
+    {
+        for(int j = 0; j < 11; j++)
+        {
+            if((i == 3 || i == 7) && (j != 3 || j != 7)) fullBoard[i][j] = '-';
+            else if(j == 3 || j == 7) fullBoard[i][j] = '|';
+            else fullBoard[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard(char (&fullBoard)[11][11])
+{
+    for(int i = 0; i < 11; i++)
+    {
+        for(int j = 0; j < 11; j++)
+        {
+            cout << fullBoard[i][j];
+        }
+        cout << endl;
+    }
+}
+
+void updateBoard(char (&fullBoard)[11][11], char (&choices)[9])
+{
+    int k = 0;
+    for(int i = 1; i < 10; i += 4)
+    {
+        for(int j = 1; j < 10; j += 4)
+        {
+            fullBoard[i][j] = choices[k];
+            k++;
+        }
+    }
 }
